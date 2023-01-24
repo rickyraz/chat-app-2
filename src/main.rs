8f93ae7568980cfdd259e7042cf4e9cff089f1c7
ricky <rickyraihan83@gmail.com>
2023-01-24 10:22:34 +0700

    muiltiple-client

--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,51 @@ async fn main() {
 
     // tcp listner
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    // exact method for tcp listener
-    let (mut socket, _addr) = listener.accept().await.unwrap();
 
-    //Split method on socket to handles i/o , read part and write part
-    let (reader, mut writer) = socket.split();
-
-    // read something from the client socket -- let mut buffer = [0u8; 1024];
-    // read some new data into memory from network stream, we need a little buffer to store the data
-    // BufReader is the same as buf reader in std lib, but more efficient and async
-    let mut reader = BufReader::new(reader);
-    // Strings to store each line
-    let mut line = String::new();
-
+    // why loop?
+    // because we want to keep accepting new connections
     loop {
-        // read data from socket
-        // let bytes_read = socket.read(&mut buffer).await.unwrap();
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        // exact method for tcp listener
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        // if no bytes read, the client has closed the connection or disconnected
-        if bytes_read == 0 {
-            break;
-        }
+        // spawn a new task for each client = ASYNC BLOCK
+        tokio::spawn(async move {
+            //Split method on socket to handles i/o , read part and write part
+            let (reader, mut writer) = socket.split();
 
-        // write data back to socket - write_all is not write a message to every sockets that's connected to TCP listener,
-        // it's  write every single byte there's in input buffer out into the output stream(buffer)
-        // very common pattern in networking = read from one socket, write to another socket
+            // read something from the client socket -- let mut buffer = [0u8; 1024];
+            // read some new data into memory from network stream, we need a little buffer to store the data
+            // BufReader is the same as buf reader in std lib, but more efficient and async
+            let mut reader = BufReader::new(reader);
+            // Strings to store each line
+            let mut line = String::new();
 
-        // socket.write_all(&buffer[0..bytes_read]).await.unwrap();
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+            loop {
+                // read data from socket
+                // let bytes_read = socket.read(&mut buffer).await.unwrap();
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                // if no bytes read, the client has closed the connection or disconnected
+                if bytes_read == 0 {
+                    break;
+                }
+
+                // write data back to socket - write_all is not write a message to every sockets that's connected to TCP listener,
+                // it's  write every single byte there's in input buffer out into the output stream(buffer)
+                // very common pattern in networking = read from one socket, write to another socket
+
+                // socket.write_all(&buffer[0..bytes_read]).await.unwrap();
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
 
 // WHY get repeated message?
 // because we are reading from the same socket, and we are not closing the connection -> line.clear()
 // it turns out that wehn you call read_line, it just appends the next line that read until string buffer
+
+// HOW to handle multiple client independently?
+// we need to spawn a new task for each client
+// standaratd way is to put excepted code in a function and call it in a loop
